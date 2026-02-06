fd556feb6dd87980e6d0378ee44ee1483032703e
GarryChen-site <chenymj23@gmail.com>
2026-02-06 19:28:55 +0900

    feat: Implement a functional echo server example with signal handling and integrate it into the CMake build system.

--- examples/echo_server.cc
+++ examples/echo_server.cc
@@ -1,6 +1,59 @@
+#include "hayai/net/EventLoop.h"
+#include "hayai/net/InetAddress.h"
+#include "hayai/net/TcpConnection.h"
+#include "hayai/net/TcpServer.h"
+#include <csignal>
 #include <iostream>
 
+using namespace hayai;
+
+EventLoop *g_loop = nullptr;
+
+void signalHandler(int) {
+  if (g_loop) {
+    g_loop->quit();
+  }
+}
+
 int main() {
-  std::cout << "Echo server example - TODO: Implement" << std::endl;
+  signal(SIGINT, signalHandler);
+  signal(SIGTERM, signalHandler);
+
+  EventLoop loop;
+  g_loop = &loop;
+
+  InetAddress listenAddr(9999);
+  TcpServer server(&loop, listenAddr, "EchoServer");
+
+  server.setConnectionCallback([](const TcpConnectionPtr &conn) {
+    if (conn->connected()) {
+      std::cout << "New connection from " << conn->peerAddress().toIpPort()
+                << std::endl;
+    } else {
+      std::cout << "Connection closed from " << conn->peerAddress().toIpPort()
+                << std::endl;
+    }
+  });
+
+  server.setMessageCallback([](const TcpConnectionPtr &conn, Buffer *buf) {
+    std::string msg = buf->retrieveAllAsString();
+    std::cout << "Received " << msg.size() << " bytes from "
+              << conn->peerAddress().toIpPort() << ": " << msg;
+
+    // Echo back
+    conn->send(msg);
+  });
+
+  // Use 4 I/O threads
+  server.setIoLoopNum(4);
+
+  std::cout << "Echo server starting on port 9999..." << std::endl;
+  std::cout << "Using 4 I/O threads" << std::endl;
+  std::cout << "Press Ctrl+C to stop" << std::endl;
+
+  server.start();
+  loop.loop();
+
+  std::cout << "\nServer stopped." << std::endl;
   return 0;
 }
